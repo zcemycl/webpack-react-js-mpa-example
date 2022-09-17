3a57fed6a98df44bbfbbaf883970d03ac5330263
Yui Chun Leung <leo.leung@faculty.ai>
2022-09-17 19:14:44 +0100

    fix ts

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     transform: {
       "^.+\\.(js|jsx)$": "babel-jest",
-      "\\.ts$": ['ts-jest']
+      // "\\.ts$": ['ts-jest']
     },
     verbose: true,
     // testEnvironment: 'node',
